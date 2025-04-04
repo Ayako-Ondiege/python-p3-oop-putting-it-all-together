1536946c235ca3fdf0c6058c7bdf3f1572c68882
Feasibo <feasibo@gmail.com>
2025-04-04 18:50:42 +0300

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title="Untitled", page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and value:
+            self._title = value
+        else:
+            raise ValueError("Title must be a non-empty string.")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
